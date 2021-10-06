343ade1b4b2065342c481a9c2d89321ef82b7627
Stephanie Zablocki <stephaniezablocki@gmail.com>
2021-10-06 10:06:17 -0400

    Added parsing function, still testing

--- BigInteger.cpp
+++ BigInteger.cpp
@@ -1,13 +1,51 @@
-#include <iostream>
+#include <iostream>;
+#include <iterator>;
+#include <regex>;
+#include <vector>;
+
 using namespace std;
 
+union Element
+{
+    int i;
+    double d;
+    char c;
+};
+
+Element *parse();
 string exp;
+
 int main()
 {
     cout << "Enter expression: ";
     getline(cin, exp);
-    cout << exp + "\n";
+    //cout << exp + "\n";
+    cout << "Length of string: " << exp.length() << "\n";
+    cout << (*parse()).c;
+    cout << "\n";
 }
-void parse()
+Element *parse()
 {
+    Element *elements = new Element[exp.length()];
+    for (int j = 0; j < exp.length(); j++)
+    {
+        Element e;
+        if (isdigit(exp[j]))
+        {
+            int n;
+            while (isdigit(exp[j]))
+            {
+                n = (n * 10) + exp[j];
+                j++;
+                cout << "num\n";
+            }
+            e.i = n;
+        }
+        else
+        {
+            e.c = exp[j];
+            cout << "char\n";
+        }
+    }
+    return elements;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+
+int main()
+{
+    cout << "Enter expression: ";
+    getline(cin, exp);
+    cout << exp + "\n";
+    cout << parse() + "\n";
+}
